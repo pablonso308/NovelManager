5c05a9b485a3f2390309db7063f4c1ab1766239d
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-10-29 18:12:32 +0100

    version 1.9 aplicar el settings con el cambio de tema

--- app/src/main/java/com/example/novelmanager/SettingsActivity.kt
+++ app/src/main/java/com/example/novelmanager/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.novelmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -8,10 +10,20 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
         supportFragmentManager
             .beginTransaction()
-            .replace(android.R.id.content, SettingsFragment())
+            .replace(R.id.settings_container, SettingsFragment())
             .commit()
+
+        val backButton: Button = findViewById(R.id.button_back)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
